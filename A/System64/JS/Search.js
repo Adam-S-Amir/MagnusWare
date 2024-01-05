0c6b2cc66b5c3b1250b31e947b3950f60848827e
Adam <64651604+Adam-S-Amir@users.noreply.github.com>
2024-01-05 04:42:49 -0500

    Major Search Update!!!
    Instead of having two arrays, a for loop cycles through the start menu and creates icons in the search menu automatically.

--- A/System64/JS/Search.js
+++ A/System64/JS/Search.js
@@ -48,36 +48,27 @@ function Search() {
     }
 
     let iconOptions = [];
+    let itemName, func, ico;
 
-    games.forEach(item => getMenuInfo(item));
-
-    function getMenuInfo(menuName) {
-        let itemName, func, ico;
-        // Iterate through the menuItems array to find the menu item with the given name
-        for (let i = 0; i < menuItems.length; i++) {
-            if (menuItems[i][0] === menuName) {
-                itemName = menuItems[i][0].replace(/\|\||\|/g, '').replace(/<br>/g, ' ');
-                func = menuItems[i][1].replace('javascript:', '');
-                ico = menuItems[i][2];
-                if (itemName) {
-                    const option = {
-                        title: itemName,
-                        src: ico,
-                        ondblclick: function () {
-                            // Perform action based on the item name
-                            eval(func);
-                        },
-                    };
-                    iconOptions.push(option);
-                } else {
-                    // Log a message if the menu item is not found
-                    console.log(`Menu Item "${menuName}" not found.`);
-                }
-                // No need to return here, just break out of the loop
-                break;
-            }
+    for (let i = 0; i < menuItems.length; i++) {
+        if (menuItems[i] && menuItems[i][1] && menuItems[i][1].includes('javascript')) {
+            itemName = menuItems[i][0].replace(/\|\||\|/g, '').replace(/<br>/g, ' ');
+            func = menuItems[i][1].replace('javascript:', '');
+            ico = menuItems[i][2];
+            console.log(itemName);
+            console.log(func);
+            console.log(ico);
+            const option = {
+                title: itemName,
+                src: ico,
+                ondblclick: function () {
+                    eval(func);
+                },
+            };
+            iconOptions.push(option);
         }
     }
+
     // Function to filter and display icons by title
     function filterIconsByTitle(searchText) {
         Programs.innerHTML = ''; // Clear the existing icons
