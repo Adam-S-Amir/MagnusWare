1f6d3391d7632c1c75a9bf2194e0efd0ced77d9f
Adam $. Amir <64651604+Adam-S-Amir@users.noreply.github.com>
2024-09-11 15:08:52 -0400

    Update filesystem

--- dir.py
+++ dir.py
@@ -5,7 +5,7 @@ def generate_dir_structure(path):
     result = {}
     for item in os.listdir(path):
         if item in ['.git', '.vscode']:
-            continue  # Skip the specified folders
+            continue
         item_path = os.path.join(path, item)
         if os.path.isdir(item_path):
             result[item] = generate_dir_structure(item_path)
